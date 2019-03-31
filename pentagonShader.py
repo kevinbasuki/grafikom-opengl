5c056b7171a768c3ea3ddbb89490ce0d2d075e31
Kevin Basuki <13516071@std.stei.itb.ac.id>
2019-03-31 22:39:16 +0700

    Add pentagon with shader

---
+++ pentagonShader.py
@@ -0,0 +1,170 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+import json
+import random
+
+# Code references = https://gist.github.com/ousttrue/c4ae334fc1505cdf4cd7
+
+class Shader(object):
+
+    def initShader(self, vertex_shader_source, fragment_shader_source):
+        # create program
+        self.program=glCreateProgram()
+        printOpenGLError()
+        # vertex shader
+        self.vs = glCreateShader(GL_VERTEX_SHADER)
+        glShaderSource(self.vs, [vertex_shader_source])
+        glCompileShader(self.vs)
+        glAttachShader(self.program, self.vs)
+        printOpenGLError()
+        # fragment shader
+        self.fs = glCreateShader(GL_FRAGMENT_SHADER)
+        glShaderSource(self.fs, [fragment_shader_source])
+        glCompileShader(self.fs)
+        glAttachShader(self.program, self.fs)
+        printOpenGLError()
+
+        glLinkProgram(self.program)
+        printOpenGLError()
+
+    def begin(self):
+        if glUseProgram(self.program):
+            printOpenGLError()
+
+    def end(self):
+        glUseProgram(0)
+
+vertices = []
+colors = [1.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0, 0.5, 0.5,  0.0, 0.0, 1.0,  0.5, 0.0, 0.5]
+indices = [0, 3, 4, 0, 1, 3, 1, 2, 3]
+
+width = 600
+height = 600
+
+buffers = None
+shader = None
+
+def init_data():
+    with open("inputPentagon.json") as json_file:
+        data = json.load(json_file)
+        i = 0
+        for key,value in data.items():
+            for key,point in value.items():
+                x = float(point[0])
+                y = float(point[1])
+                wd = (width+50)/2
+                ht = (height+50)/2
+
+                vertices.append((x-wd)/wd)
+                vertices.append((y-ht)/ht)
+                vertices.append(0.0)
+
+def initialize():
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glDepthFunc(GL_LESS)
+    glEnable(GL_DEPTH_TEST)
+
+def resizeWindow(Width, Height):
+    # viewport
+    if Height == 0:
+        Height = 1
+    glViewport(0, 0, Width, Height)
+    # projection
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+
+def draw():
+    global yaw, pitch
+    # clear
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    # view
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glTranslatef(0.0, 0.0, -2.0)
+    # unicorn
+    draw_unicorn()
+
+    glFlush()
+
+def printOpenGLError():
+    err = glGetError()
+    if (err != GL_NO_ERROR):
+        print('GLERROR: ', gluErrorString(err))
+        #sys.exit()
+
+def create_vbo():
+    buffers = glGenBuffers(3)
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0])
+    glBufferData(GL_ARRAY_BUFFER, 
+            len(vertices)*4,  # byte size
+            (ctypes.c_float*len(vertices))(*vertices), 
+            GL_STATIC_DRAW)
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[1])
+    glBufferData(GL_ARRAY_BUFFER, 
+            len(colors)*4, # byte size 
+            (ctypes.c_float*len(colors))(*colors), 
+            GL_STATIC_DRAW)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[2])
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 
+            len(indices)*4, # byte size
+            (ctypes.c_uint*len(indices))(*indices), 
+            GL_STATIC_DRAW)
+    return buffers
+
+def draw_vbo():
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glEnableClientState(GL_COLOR_ARRAY)
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0])
+    glVertexPointer(3, GL_FLOAT, 0, None)
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[1])
+    glColorPointer(3, GL_FLOAT, 0, None)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[2])
+    glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
+    glDisableClientState(GL_COLOR_ARRAY)
+    glDisableClientState(GL_VERTEX_ARRAY)
+
+def draw_unicorn():
+    global shader, buffers
+    if shader==None:
+        shader=Shader()
+        shader.initShader('''
+void main()
+{
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    gl_FrontColor = gl_Color;
+}
+        ''',
+        '''
+void main()
+{
+    gl_FragColor = gl_Color;
+}
+        ''')
+        buffers=create_vbo()
+
+    shader.begin()
+    draw_vbo()
+    shader.end()
+
+def reshape_func(w, h):
+    resizeWindow(w, h == 0 and 1 or h)
+
+def disp_func():
+    draw()
+    glutSwapBuffers()
+
+if __name__=="__main__":
+    init_data()
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+    glutInitWindowSize(width,height)
+    glutCreateWindow("vbo")
+    glutDisplayFunc(disp_func)
+    glutIdleFunc(disp_func)
+    glutReshapeFunc(reshape_func)
+    initialize()
+    glutMainLoop()
