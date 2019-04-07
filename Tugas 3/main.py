55a00eec35792f295978e251a3afdc2589826e35
Dicky Adrian <dickyadrian0204@gmail.com>
2019-04-07 17:17:15 +0700

    Add Tugas3 not done

---
+++ Tugas 3/main.py
@@ -0,0 +1,172 @@
+import glfw
+from OpenGL.GL import *
+import numpy
+from pyrr import matrix44
+from Camera import Camera
+from ShaderLoader import *
+
+def window_resize(window, width, height):
+    glViewport(0, 0, width, height)
+
+cam = Camera()
+keys = [False] * 1024
+lastX, lastY = 640, 360
+first_mouse = True
+
+def key_callback(window, key, scancode, action, mode):
+    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+        glfw.set_window_should_close(window, True)
+
+    if key >= 0 and key < 1024:
+        if action == glfw.PRESS:
+            keys[key] = True
+        elif action == glfw.RELEASE:
+            keys[key] = False
+
+
+def do_movement():
+    if keys[glfw.KEY_W]:
+        cam.process_keyboard("FORWARD", 0.05)
+    if keys[glfw.KEY_S]:
+        cam.process_keyboard("BACKWARD", 0.05)
+    if keys[glfw.KEY_A]:
+        cam.process_keyboard("LEFT", 0.05)
+    if keys[glfw.KEY_D]:
+        cam.process_keyboard("RIGHT", 0.05)
+
+
+def mouse_callback(window, xpos, ypos):
+    global first_mouse, lastX, lastY
+
+    if first_mouse:
+        lastX = xpos
+        lastY = ypos
+        first_mouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos
+
+    lastX = xpos
+    lastY = ypos
+
+    cam.process_mouse_movement(xoffset, yoffset)
+
+
+def main():
+    # initialize glfw
+    if not glfw.init():
+        return
+
+    w_width, w_height = 1280, 720
+    aspect_ratio = w_width / w_height
+
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 2)
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+    window = glfw.create_window(w_width, w_height, "My OpenGL window", None, None)
+
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    glfw.set_window_size_callback(window, window_resize)
+    glfw.set_key_callback(window, key_callback)
+    glfw.set_cursor_pos_callback(window, mouse_callback)
+
+    #        positions        texture_coords
+    cube = [-0.5, -0.5, 0.5, 0.0, 0.0,
+            0.5, -0.5, 0.5, 1.0, 0.0,
+            0.5, 0.5, 0.5, 1.0, 1.0,
+            -0.5, 0.5, 0.5, 0.0, 1.0,
+
+            -0.5, -0.5, -0.5, 0.0, 0.0,
+            0.5, -0.5, -0.5, 1.0, 0.0,
+            0.5, 0.5, -0.5, 1.0, 1.0,
+            -0.5, 0.5, -0.5, 0.0, 1.0,
+
+            0.5, -0.5, -0.5, 0.0, 0.0,
+            0.5, 0.5, -0.5, 1.0, 0.0,
+            0.5, 0.5, 0.5, 1.0, 1.0,
+            0.5, -0.5, 0.5, 0.0, 1.0,
+
+            -0.5, 0.5, -0.5, 0.0, 0.0,
+            -0.5, -0.5, -0.5, 1.0, 0.0,
+            -0.5, -0.5, 0.5, 1.0, 1.0,
+            -0.5, 0.5, 0.5, 0.0, 1.0,
+
+            -0.5, -0.5, -0.5, 0.0, 0.0,
+            0.5, -0.5, -0.5, 1.0, 0.0,
+            0.5, -0.5, 0.5, 1.0, 1.0,
+            -0.5, -0.5, 0.5, 0.0, 1.0,
+
+            0.5, 0.5, -0.5, 0.0, 0.0,
+            -0.5, 0.5, -0.5, 1.0, 0.0,
+            -0.5, 0.5, 0.5, 1.0, 1.0,
+            0.5, 0.5, 0.5, 0.0, 1.0]
+
+    cube = numpy.array(cube, dtype=numpy.float32)
+    indices = [0, 1, 2, 2, 3, 0,
+               4, 5, 6, 6, 7, 4,
+               8, 9, 10, 10, 11, 8,
+               12, 13, 14, 14, 15, 12,
+               16, 17, 18, 18, 19, 16,
+               20, 21, 22, 22, 23, 20]
+
+    indices = numpy.array(indices, dtype=numpy.uint32)
+
+    program = compile_shader("Shaders/CarShader.vs", "Shaders/CarShader.fs")
+
+    VAO = glGenVertexArrays(1)
+    glBindVertexArray(VAO)
+
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, cube.itemsize * len(cube)-2, cube, GL_STATIC_DRAW)
+
+    EBO = glGenBuffers(1)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.itemsize * len(indices), indices, GL_STATIC_DRAW)
+
+    # position
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, cube.itemsize * 5, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(0)
+
+    glUseProgram(program)
+
+    glClearColor(0.2, 0.3, 0.2, 1.0)
+    glEnable(GL_DEPTH_TEST)
+
+    projection = matrix44.create_perspective_projection_matrix(45.0, aspect_ratio, 0.1, 100.0)
+
+    model_loc = glGetUniformLocation(program, "model")
+    view_loc = glGetUniformLocation(program, "view")
+    proj_loc = glGetUniformLocation(program, "proj")
+
+    cube_positions = [(0.0, 0.0, 0.0), (1.0, 0.0, 0.0), (1.5, -1.2, -2.5), (8.8, -2.0, -12.3), (-2.0, 2.0, -5.5),
+                      (-4.0, 2.0, -3.0)]
+
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+        do_movement()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        view = cam.get_view_matrix()
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+
+        for i in range(len(cube_positions)):
+            model = matrix44.create_from_translation(cube_positions[i])
+            glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
+            glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
+
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+
+if __name__ == "__main__":
+    main()
