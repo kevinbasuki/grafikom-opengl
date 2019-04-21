ef85170aedad0ef31c191e51188891dbf7aec6b5
Dicky Adrian <dickyadrian0204@gmail.com>
2019-04-21 17:59:48 +0700

    Add diffuse lighting, Delete specular lighting

--- Tugas 5/Shaders/CarShader.fs
+++ Tugas 5/Shaders/CarShader.fs
@@ -1,7 +1,7 @@
 #version 330
 
-uniform vec4 color_ambient = vec4(0.1, 0.1, 0.1, 1.0);
-uniform vec4 color_diffuse = vec4(0.1, 0.1, 0.1, 0.1);
+uniform vec4 color_ambient = vec4(1.0, 1.0, 1.0, 1.0);
+uniform vec4 color_diffuse = vec4(1.0, 1.0, 1.0, 1.0);
 uniform vec4 color_specular = vec4(1, 1, 1, 1.0);
 uniform float shininess = 50.0f;
 
@@ -18,13 +18,13 @@ void main()
 {
     vec3 light_direction = normalize(light_position - position_vs);
     vec3 normal = normalize(normal_vs);
-    vec3 half_vector = normalize(light_direction + normalize(position_vs));
-    float diffuse = max(0.0, dot(normal, light_direction));
-    float specular = pow(max(0.0, dot(normal_vs, half_vector)), shininess);
-
+    
     float ambientStrength = 0.1;
-
-    vec4 objectColor = texture(tex_sampler, textures);
     vec4 ambient = ambientStrength * color_ambient;
-    color = color_ambient + objectColor * color_diffuse + specular * color_specular;
+
+    float diff = max(dot(normal, light_direction), 0.0);
+    vec4 diffuse = diff * color_diffuse;
+
+    vec4 objectColor = (ambient+diffuse) * texture(tex_sampler, textures);
+    color = objectColor;
 }
--- Tugas 5/Shaders/CarShader.vs
+++ Tugas 5/Shaders/CarShader.vs
@@ -1,5 +1,5 @@
 #version 330
-layout(location = 0) in vec3 position;
+layout(location = 0) in vec3 inposition;
 layout(location = 1) in vec2 texture_cords;
 layout(location = 2) in vec3 normal;
 
@@ -13,10 +13,9 @@ out vec3 normal_vs;
 
 void main()
 {
-    vec4 position = proj * view * model * vec4(position, 1.0f);
-    gl_Position =  position;
-
+    gl_Position = proj * view * model * vec4(inposition, 1.0f);
+    
     textures = texture_cords;
-    position_vs = position.xyz;
-    normal_vs = normal;
+    position_vs = vec3(model * vec4(inposition, 1.0f));
+    normal_vs = mat3(transpose(inverse(model))) * normal;
 }
